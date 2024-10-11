3e6c803e6978ac9af0e742b98eaf2aa235b74b3c
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2024-10-11 15:36:28 -0300

    Set coverage minimum to 75%

--- tests/check_coverage.py
+++ tests/check_coverage.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     with open("coverage.json") as file:
         coverage_info = json.load(file)
 
-    assert coverage_info["totals"]["percent_covered"] > 78
+    assert coverage_info["totals"]["percent_covered"] > 75
