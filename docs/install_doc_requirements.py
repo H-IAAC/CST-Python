a9d6ae2d2452b959e45e6574cafb624c428db805
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2024-11-26 14:23:07 -0300

    Documentation workflow

--- docs/install_doc_requirements.py
+++ docs/install_doc_requirements.py
@@ -1,10 +1,11 @@
 import subprocess
 import sys
-import configparser
+import tomllib
 import os
 
-config = configparser.ConfigParser()
-config.read(os.path.join("..", "setup.cfg"))
-packages = config["options.extras_require"]["doc_generation"]
+with open("../pyproject.toml", "rb") as file:
+    data = tomllib.load(file)
 
-subprocess.check_call([sys.executable, "-m", "pip", "install"]+packages.split(";"))
+packages = data["project"]["optional-dependencies"]["doc_generation"]
+
+subprocess.check_call([sys.executable, "-m", "pip", "install"]+packages)
